500554d43bf17b332cae8c9530c1623876eccb9b
Igor Goltsov <riversy@gmail.com>
2020-08-04 01:28:23 +0300

    Add Semantic-UI and update Main App Component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('voting-app-edu app is running!');
+    expect(compiled.querySelector('.header h1.header').textContent).toContain('Angular 2 Simple Reddit');
   });
 });
